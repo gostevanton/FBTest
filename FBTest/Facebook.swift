99d387923fb91b11408c7484967c621c0ea4035c
Антон Гостев <antongostev@MacBook-Air-Anton.local>
2017-05-09 12:46:42 +0300

    Add facebook class

---
+++ FBTest/Facebook.swift
@@ -0,0 +1,23 @@
+//
+//  Facebook.swift
+//  FBTest
+//
+//  Created by Антон Гостев on 09.05.17.
+//  Copyright © 2017 Антон Гостев. All rights reserved.
+//
+
+import UIKit
+
+class Facebook {
+    
+    /**
+     Get facebook access token by opening a login window.
+     
+     - parameter callback:      Returns AccessToken as String or an Error.
+     */
+    public func getAuthToken(callback: @escaping (String?, Error?) -> Void) {
+        // TODO(кандидат): определить метод. Функция, которая делает логин:
+        // LoginManager().logIn([.publicProfile, .email], viewController: someViewController, someCallback)
+    }
+
+}
