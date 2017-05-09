4ffa8f7b20c5e64ac5dfd332dc24e3c07790cb02
Антон Гостев <antongostev@MacBook-Air-Anton.local>
2017-05-09 12:48:18 +0300

    Create presenter with protocol

---
+++ FBTest/Presenter.swift
@@ -0,0 +1,13 @@
+//
+//  Presenter.swift
+//  FBTest
+//
+//  Created by Антон Гостев on 09.05.17.
+//  Copyright © 2017 Антон Гостев. All rights reserved.
+//
+
+import UIKit
+
+protocol Presenter {
+
+}
---
+++ FBTest/PresenterImpl.swift
@@ -0,0 +1,13 @@
+//
+//  PresenterImpl.swift
+//  FBTest
+//
+//  Created by Антон Гостев on 09.05.17.
+//  Copyright © 2017 Антон Гостев. All rights reserved.
+//
+
+import UIKit
+
+class PresenterImpl: Presenter {
+
+}
